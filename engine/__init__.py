1931200e3a066ce7cc7b1bc8031d6fb338605841
andrefcsj <31588520+andrefcsj@users.noreply.github.com>
2026-07-09 20:59:03 -0300

    Sprint 1.1-R: expose engine foundation API

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,1 +1,6 @@
-"""FaculdadeMaria Decision Engine package."""
+"""Public foundation API for the FaculdadeMaria Decision Engine."""
+
+from .errors import DecisionEngineError
+from .version import ENGINE_VERSION, get_engine_version
+
+__all__ = ['DecisionEngineError', 'ENGINE_VERSION', 'get_engine_version']
