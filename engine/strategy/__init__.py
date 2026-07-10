112be306ce07b0ed27d4ffaebead32910a1674fc
andrefcsj <31588520+andrefcsj@users.noreply.github.com>
2026-07-10 16:23:19 -0300

    feat(engine): expose PUT strategy API

---
+++ engine/strategy/__init__.py
@@ -0,0 +1,21 @@
+from .put import (
+    ELIGIBLE,
+    INELIGIBLE,
+    INSUFFICIENT_DATA,
+    WATCHLIST,
+    PutStrategyConfig,
+    PutStrategyEvaluation,
+    StrategyCheck,
+    evaluate_put_strategy,
+)
+
+__all__ = [
+    "ELIGIBLE",
+    "INELIGIBLE",
+    "INSUFFICIENT_DATA",
+    "WATCHLIST",
+    "PutStrategyConfig",
+    "PutStrategyEvaluation",
+    "StrategyCheck",
+    "evaluate_put_strategy",
+]
