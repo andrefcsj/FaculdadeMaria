8f97bee54d633661d44478b383e182c0d3bf381c
andrefcsj <31588520+andrefcsj@users.noreply.github.com>
2026-07-10 16:23:33 -0300

    feat(engine): expose Sprint C public API

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,5 +1,6 @@
 """Public API for the FaculdadeMaria Decision Engine."""
 
+from .asset import AssetQualityAssessment, AssetQualityPolicy, AssetQualityProfile, assess_asset_quality
 from .core import OptionOpportunity
 from .errors import DecisionEngineError
 from .filters import SafetyFilterConfig, SafetyEvaluation, evaluate_put_safety
@@ -16,21 +17,29 @@ from .indicators import (
     true_range,
 )
 from .metrics import PutMetricAssumptions, PutMetrics, calculate_put_metrics
+from .strategy import PutStrategyConfig, PutStrategyEvaluation, evaluate_put_strategy
 from .version import ENGINE_VERSION, get_engine_version
 
 __all__ = [
+    "AssetQualityAssessment",
+    "AssetQualityPolicy",
+    "AssetQualityProfile",
     "BollingerBands",
     "DecisionEngineError",
     "ENGINE_VERSION",
     "OptionOpportunity",
     "PutMetricAssumptions",
     "PutMetrics",
+    "PutStrategyConfig",
+    "PutStrategyEvaluation",
     "SafetyEvaluation",
     "SafetyFilterConfig",
+    "assess_asset_quality",
     "average_true_range",
     "bollinger_bands",
     "calculate_put_metrics",
     "evaluate_put_safety",
+    "evaluate_put_strategy",
     "get_engine_version",
     "historical_volatility",
     "moving_average_21",
