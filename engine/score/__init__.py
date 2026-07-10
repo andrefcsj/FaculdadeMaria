e86ce57401d158113a0cb67ae87320651f401945
andrefcsj <31588520+andrefcsj@users.noreply.github.com>
2026-07-10 16:47:45 -0300

    feat(engine): add explainable score package

---
+++ engine/score/__init__.py
@@ -0,0 +1,15 @@
+from .explainable import (
+    DEFAULT_TARGET_GROSS_ROI,
+    ExplainableScoreConfig,
+    ScoreComponent,
+    ScoreEvaluation,
+    calculate_put_score,
+)
+
+__all__ = [
+    "DEFAULT_TARGET_GROSS_ROI",
+    "ExplainableScoreConfig",
+    "ScoreComponent",
+    "ScoreEvaluation",
+    "calculate_put_score",
+]
